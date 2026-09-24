89cfea36a229559066733783dc4b6403b2ee542f
webAnkit23 <www.ankitsingh2323@gmail.com>
2026-09-24 22:05:37 +0530

    last commit

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,28 +1,63 @@
-
 const mongoose = require("mongoose");
 
 const EventSchema = new mongoose.Schema({
-    name : {
-        type :String,
-        required : true
+
+    name: {
+        type: String,
+        required: true
     },
-    description : {
-        type :String,
-        required : true
+
+    description: {
+        type: String,
+        required: true
     },
-    minPlayer : {
-        type : Number,
-        required : true,
-        min : 1
+
+    minPlayer: {
+        type: Number,
+        required: true,
+        min: 1
     },
-    maxPlayer : {
-        type : Number,
-        required :true,
-        min : 1
+
+    maxPlayer: {
+        type: Number,
+        required: true,
+        min: 1
     },
-    registrationOpen : {
-        type : Boolean,
-        default : true
+
+    registrationOpen: {
+        type: Boolean,
+        default: true
     },
+
+    // Technical / Non-Technical
+    type: {
+        type: String,
+        enum: ["Technical", "Non-Technical"],
+        required: true
+    },
+
+    // Venue
+    venue: {
+        type: String,
+        required: true
+    },
+
+    // Timeline
+    date: {
+        type: Date,
+        required: true
+    },
+
+    startTime: {
+        type: String,
+        required: true
+    },
+
+    endTime: {
+        type: String,
+        required: true
+    }
+
 });
-module.exports  = mongoose.model("Event", EventSchema);
+
+module.exports = mongoose.model("Event", EventSchema);
